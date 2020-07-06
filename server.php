e9c455bec716b15cae1e5eef9ca860a22a79ec97
saranraj0018 <67306654+saranraj0018@users.noreply.github.com>
2020-07-06 09:04:54 -0700

    Add files via upload

--- server.php
+++ server.php
@@ -13,11 +13,10 @@ function evenCmp($input)
 } 
 $odd = array_filter($input, "oddCmp"); 
 $even = array_filter($input, "evenCmp"); 
-$odd = array_values(array_filter($odd)); 
-$even = array_values(array_filter($even)); 
-print"Odd array :"; 
+
+
 print_r($odd); 
-print"Even array :"; 
 print_r($even); 
-  
+echo "<br>";
+print_r(array_merge($even,$odd));
 ?> 
